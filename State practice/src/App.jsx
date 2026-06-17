adcd5b2c3837639f23c80dc8773a91d0b117df3a
Tajudeen Quadri <tajudeenquadri590@gmail.com>
2026-06-17 19:15:36 +0100

    add Todolist

--- State practice/src/App.jsx
+++ State practice/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Counter from './Components/Counter'
+import Todolist from './Components/Todolist'
 
 const App = () => {
   return (
     <div>
       <Counter />
+      <Todolist/>
+      
     </div>
   )
 }
---
+++ State practice/src/Components/Todolist.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+
+const Todolist = () => {
+    const[Todo,setTodo]=useState([]);
+  return (
+    <div>
+        <h1>Todolist</h1>
+        <input type="text" />
+
+    </div>
+  )
+}
+
+export default Todolist
