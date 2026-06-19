2c82e3f4625c3ee96b6203635678deabdb213cf7
Tajudeen Quadri <tajudeenquadri590@gmail.com>
2026-06-19 19:01:34 +0100

    obect state added

--- State practice/src/App.jsx
+++ State practice/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Counter from './Components/Counter'
 import Todolist from './Components/Todolist'
+import Profile from './Components/profile'
 
 const App = () => {
   return (
     <div>
       <Counter />
       <Todolist/>
-      
+      <Profile />
     </div>
   )
 }
---
+++ State practice/src/Components/Profile.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Profile= () => {
+
+    const[userInfo,setUserInfo]=useState({
+        name:"Tajudeen Quadri",
+        age:15,
+        hobby:"Footballing"
+    })
+  return (
+    <div>
+        <h1>Profile information</h1>
+
+        <h2>Name:{userInfo.name}</h2>
+        <h2>Age:{userInfo.age}</h2>
+        <h2>Hobby:{userInfo.hobby}</h2>
+
+       <form action="" style={{display:"flex",flexDirection:"column",gap:"10px", marginTop:"20px",width:"30%"}}>
+        <input  type="text" placeholder='Enter your name' onChange={(e)=>setUserInfo({...userInfo,name:e.target.value})} />
+
+        <input type="text" placeholder='Enter your age' onChange={(e)=>setUserInfo({...userInfo,age:e.target.value})} />
+        <input type="text" placeholder='Enter your hobby' onChange={(e)=>setUserInfo({...userInfo,hobby:e.target.value})} />
+
+        </form>
+    </div>
+  )
+}
+
+export default Profile
