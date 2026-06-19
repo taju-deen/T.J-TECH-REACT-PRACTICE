833ef2e60cc20000f896b571ec2e990bf6977ca7
Tajudeen Quadri <tajudeenquadri590@gmail.com>
2026-06-19 18:18:53 +0100

    Todolist practice done

--- State practice/src/Components/Todolist.jsx
+++ State practice/src/Components/Todolist.jsx
@@ -2,11 +2,36 @@ import { useState } from "react"
 
 
 const Todolist = () => {
+    const [inputValue,setInputValue]=useState("");
     const[Todo,setTodo]=useState([]);
+
+  const handleChange=(e)=>{
+   const show= e.target.value;
+
+    setInputValue(show);
+  }
+
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    setTodo([...Todo,inputValue]);
+    setInputValue("");
+}
+
+
+
   return (
     <div>
         <h1>Todolist</h1>
-        <input type="text" />
+        <form action="" onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} placeholder="Enter Your name" onChange={handleChange} />
+        
+        <button  >Press me</button>
+        </form>
+        <ul>
+          {Todo.map((item,index)=>(
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
 
     </div>
   )
